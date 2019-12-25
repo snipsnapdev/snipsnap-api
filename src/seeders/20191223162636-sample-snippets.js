64cead5f626a3b7622cf5581242187fbfade7f8c
Kirill Bolotsky <frontend@bolotsky.dev>
2019-12-25 12:38:44 +0500

    introduced a few json snippets files

--- src/seeders/20191223162636-sample-snippets.js
+++ src/seeders/20191223162636-sample-snippets.js
@@ -1,26 +1,38 @@
+const reactSampleSnippets = require('react.json');
+const nodeSampleSnippets = require('node.json');
+const gatsbySampleSnippets = require('gatsby.json');
+
+console.log(d);
 
 module.exports = {
-  up: (queryInterface) => queryInterface.bulkInsert('Snippets', [{
-    name: 'gatsby',
-    language: 'javascript',
-    snippets: '{"test": 1}',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    name: 'gatsby-image',
-    language: 'javascript',
-    snippets: '{"test": 1}',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  {
-    name: 'express',
-    language: 'javascript',
-    snippets: '{"test": 1}',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }], {}),
+  up: queryInterface =>
+    queryInterface.bulkInsert(
+      'Snippets',
+      [
+        {
+          name: 'gatsby',
+          language: 'javascript',
+          snippets: JSON.stringify(gatsbySampleSnippets),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'react',
+          language: 'javascriptreact',
+          snippets: reactSampleSnippets,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'node',
+          language: 'javascript',
+          snippets: nodeSampleSnippets,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    ),
 
-  down: (queryInterface) => queryInterface.bulkDelete('Snippets', null, {}),
+  down: queryInterface => queryInterface.bulkDelete('Snippets', null, {})
 };

0cf35772743f69a47f76721133c74f6185f65f85
Kirill Bolotsky <frontend@bolotsky.dev>
2019-12-25 21:19:48 +0500

    hotfix: removed unnecessary stringify

--- src/helpers/createSnippetsOutput.js
+++ src/helpers/createSnippetsOutput.js
@@ -1,14 +1,13 @@
 // takes the db snippets query result in JSON and formats it as a proper
 // snipsnap.code-snippets file
 // createSnippetsOutput(queryResult: Array = []) -> JSON
-const createSnippetsOuput = (queryResult = []) => JSON.stringify(
+const createSnippetsOuput = (queryResult = []) =>
   queryResult
     .map(JSON.stringify)
     .map(JSON.parse)
     .map(({ snippets }) => snippets)
-  // logic for name spacing the snippets like gatsby-image__imageQuerySnippet
-  // could be added here
-    .reduce((storage, snippet) => ({ ...storage, ...snippet }), {}),
-);
+    // logic for name spacing the snippets like gatsby-image__imageQuerySnippet
+    // could be added here
+    .reduce((storage, snippet) => ({ ...storage, ...snippet }), {});
 
 module.exports = createSnippetsOuput;

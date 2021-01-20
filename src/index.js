cb091865e5039ab297356e65944bf042547ab7da
Alex Step <to@alexstep.com>
2021-01-20 21:13:06 +0300

    Descrease dockerimage size

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const { logger } = require('./middlewares/logger');
 
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen((process.env.PORT || 8000), () => {
   const { port } = server.address();
   logger.info(`Listening on port ${port}`);
 });
